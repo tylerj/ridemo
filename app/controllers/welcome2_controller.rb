04d74910514188925c5d0c487e5144eb9fe6ebeb
Tyler Johnson <tylerl.johnson@gmail.com>
2011-06-26 18:52:20 -0400

    Initial commit

---
+++ app/controllers/welcome2_controller.rb
@@ -0,0 +1,2 @@
+class Welcome2Controller < ApplicationController
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ridemo::Application.config.secret_token = '7bb013189fc2ce9702d1836ee2450ea35595ca16c5664bc71ab357ca037dec3d877d4011ade17c96b4c38457567f7ee9578af2b73fb0920c11c3dd1830e4736d'
